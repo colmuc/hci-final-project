d5e69940e70e9eccca0dd6e9ff758d74b19da020
Collins Mucyo <collinsmucyo@gmail.com>
2019-04-22 17:50:58 -0500

    Added Search Bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -28,6 +28,17 @@ const NavBar = () => {
             </Link>
           </li>
         </ul>
+        <form class="form-inline my-3 my-lg-0 align-right">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
       </div>
     </nav>
   );
